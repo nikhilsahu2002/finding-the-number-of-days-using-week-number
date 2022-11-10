c1dcfe6b92af2e3e33e60cc1c87afe202efa895e
nikhilsahu2002 <117375980+nikhilsahu2002@users.noreply.github.com>
2022-11-10 23:50:30 +0530

    Update WEEKDAYS.C

--- WEEKDAYS.C
+++ WEEKDAYS.C
@@ -2,11 +2,25 @@
 #include<conio.h>
 void main()
 {
-int i;
-char array[][20]={"a","b","c","d","e"};
+int week;
 clrscr();
-for (i=0;i<3;i++)
-	printf("%s\n",array[i]);
+printf("Enter Week Nuber (1-7)");
+scanf("%d",&week);
+if(week == 1)
+printf("Monday");
+else if(week==2)
+printf("tuseday");
+else if(week==3)
+printf("Wedneday");
+else if(week==4)
+printf("thusday");
+else if(week==5)
+printf("friday");
+else if(week==6)
+printf("saturday");
+else if(week==7)
+printf("sunday");
+else
+printf("Please enter valid input between 1 to 7");
 getch();
-
 }
